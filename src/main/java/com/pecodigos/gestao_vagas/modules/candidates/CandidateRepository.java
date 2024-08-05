234f9001e03c5a069f8df97c6407858ca9939b25
pecodigos <pedrenriquecunha@gmail.com>
2024-08-05 06:32:51 -0300

    Implemented some entities, CandidateController and its useCase and some exception handlers

---
+++ src/main/java/com/pecodigos/gestao_vagas/modules/candidates/CandidateRepository.java
@@ -0,0 +1,10 @@
+package com.pecodigos.gestao_vagas.modules.candidates;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+}
---
+++ src/main/java/com/pecodigos/gestao_vagas/modules/candidates/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,23 @@
+package com.pecodigos.gestao_vagas.modules.candidates.useCases;
+
+import com.pecodigos.gestao_vagas.exceptions.UserFoundException;
+import com.pecodigos.gestao_vagas.modules.candidates.CandidateEntity;
+import com.pecodigos.gestao_vagas.modules.candidates.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+        return this.candidateRepository.save(candidateEntity);
+    }
+}
