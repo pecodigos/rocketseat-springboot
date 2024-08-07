71cb72bbd88d078e57fad0c49a4f1614c6bbba50
pecodigos <pedrenriquecunha@gmail.com>
2024-08-07 17:34:09 -0300

    Created ProfileCandidateUseCase and its DTO, and implemented some changes

--- src/main/java/com/pecodigos/gestao_vagas/modules/candidates/controllers/CandidateController.java
+++ src/main/java/com/pecodigos/gestao_vagas/modules/candidates/controllers/CandidateController.java
@@ -2,17 +2,21 @@ package com.pecodigos.gestao_vagas.modules.candidates.controllers;
 
 import com.pecodigos.gestao_vagas.modules.candidates.CandidateEntity;
 import com.pecodigos.gestao_vagas.modules.candidates.useCases.CreateCandidateUseCase;
+import com.pecodigos.gestao_vagas.modules.candidates.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
+    @Autowired
+    private ProfileCandidateUseCase profileCandidateUseCase;
 
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
@@ -24,6 +28,18 @@ public class CandidateController {
             return ResponseEntity.ok().body(result);
         }
         catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> get(HttpServletRequest request) {
+
+        var candidateId = request.getAttribute("candidate_id").toString();
+        try {
+            var profile = this.profileCandidateUseCase.execute(UUID.fromString(candidateId));
+            return ResponseEntity.ok().body(profile);
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
---
+++ src/main/java/com/pecodigos/gestao_vagas/modules/candidates/useCases/ProfileCandidateUseCase.java
@@ -0,0 +1,30 @@
+package com.pecodigos.gestao_vagas.modules.candidates.useCases;
+
+import com.pecodigos.gestao_vagas.modules.candidates.CandidateRepository;
+import com.pecodigos.gestao_vagas.modules.candidates.dto.ProfileCandidateResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ProfileCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public ProfileCandidateResponseDTO execute(UUID candidateId) {
+        var candidate = this.candidateRepository.findById(candidateId)
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("User not found.");
+        });
+        return ProfileCandidateResponseDTO.builder()
+                .description(candidate.getDescription())
+                .username(candidate.getUsername())
+                .email(candidate.getEmail())
+                .name(candidate.getName())
+                .id(candidate.getId())
+                .build();
+    }
+}
