ba1ae77de1e53688751331291e584b9d8ad965dd
pecodigos <pedrenriquecunha@gmail.com>
2024-08-09 09:32:43 -0300

    implementing applyJob method on CandidateController

--- src/main/java/com/pecodigos/gestao_vagas/modules/candidates/controllers/CandidateController.java
+++ src/main/java/com/pecodigos/gestao_vagas/modules/candidates/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package com.pecodigos.gestao_vagas.modules.candidates.controllers;
 
 import com.pecodigos.gestao_vagas.modules.candidates.entities.CandidateEntity;
+import com.pecodigos.gestao_vagas.modules.candidates.useCases.ApplyJobCandidateUseCase;
 import com.pecodigos.gestao_vagas.modules.candidates.useCases.CreateCandidateUseCase;
 import com.pecodigos.gestao_vagas.modules.candidates.useCases.ListAllJobsByFilterUseCase;
 import com.pecodigos.gestao_vagas.modules.candidates.useCases.ProfileCandidateUseCase;
@@ -36,6 +37,9 @@ public class CandidateController {
 
     @Autowired
     private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
+
+    @Autowired
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
 
     @PostMapping("/")
     @Operation(summary = "Cadastro de candidato", description = "Essa função é responsável por cadastrar o candidato")
@@ -89,5 +93,20 @@ public class CandidateController {
     @SecurityRequirement(name = "jwt_auth")
     public List<JobEntity> findJobByFilter(@RequestParam String filter) {
         return this.listAllJobsByFilterUseCase.execute(filter);
+    }
+
+    @PostMapping("/job/apply")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Operation(summary = "Inscrição do candidato para uma vaga", description = "Essa função é responsável por realizar a inscrição do candidato em uma vaga.")
+    @SecurityRequirement(name = "jwt_auth")
+    public ResponseEntity<Object> applyJob(HttpServletRequest request, @RequestBody UUID jobId) {
+        var candidateId = request.getAttribute("candidate_id");
+
+        try {
+            var result = this.applyJobCandidateUseCase.execute(UUID.fromString(candidateId.toString()), jobId);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
